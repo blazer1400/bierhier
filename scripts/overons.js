286071e569ca363efad0f370b7886c67ddd1c302
Stan <sbroersma@roc-dev.com>
2020-12-15 15:09:34 +0100

    over ons

---
+++ scripts/overons.js
@@ -0,0 +1,16 @@
+var output = document.getElementById('persoon');
+function tristan() {
+    output.innerHTML = "<img src='img/tris.png'><h2>Tristan</h2> <p>Ik ben zwart</p>";
+}
+function sem() {
+    output.innerHTML = "<img src=''><h2>Sem</h2> <p>Ik ben zwart</p>";
+}
+function chester() {
+    output.innerHTML = "<img src=''><h2>Chester</h2> <p>Ik ben zwart</p>";
+}
+function rory() {
+    output.innerHTML = "<img src='img/rory.png'><h2>Rory</h2> <p>Ik ben zwart</p>";
+}
+function stan() {
+    output.innerHTML = "<img src=''><h2>Stan</h2> <p>kaaskroketmaster35superlitman</p>";
+}
